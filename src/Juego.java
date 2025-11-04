e76396374283ad590b50f6836ea59bb7cef9d97f
yerayduran <yerayduranchaves2006@gmail.com>
2025-11-04 12:14:02 +0100

    Estructura del numero de habitaciones

--- src/Juego.java
+++ src/Juego.java
@@ -8,8 +8,9 @@ import java.util.Scanner;
 
 public class Juego {
 
-    // --- NÚCLEO: Definición de Datos (FASE 1) ---
-    // Esta parte os la damos HECHA. Es el "contrato" del núcleo.
+
+
+
 
     private static String descripcionJuego = "Estabas cagando en el baño de un mercadona random, tenias mucho sueño porque habias estado toda la noche jugando al call of duty, de repente notas cómo tus parpados empiezan a cerrarse. Al final te quedas dormido por viciar toda la noche y resulta que, cuando te despiertas, en vez de estar sentado en aquel váter estas en una especie de sillón en una sala totalmente a oscuras. Cuando te levantas, se te enciende la sala en la que estás. Ya no estás en aquel baño cutre del mercadona, ahora estabas en un lugar totalmente desconocido. ";
 
@@ -40,9 +41,12 @@ public class Juego {
 
 
     public static void main(String[] args) {
-        // Puedes utilizar la clase MiEntradaSalida, que viviría en el paquete io
         Scanner scanner = new Scanner(System.in);
         boolean jugando = true;
+        char adelante = 'W';
+        char atras = 'S';
+        char derecha = 'D';
+        char izquierda = 'A';
 
         System.out.println("¡Bienvenido a 'TU PROPIA AVENTURA'!");
         System.out.println("------------------------------------------");
